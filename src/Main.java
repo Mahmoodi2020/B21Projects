0ce6f5722416794d6ea352c4c64cd3881c075f41
Mahmoodik <mahmoodik@vcu.edu>
2020-11-12 17:40:11 -0500

    Project is already in remote repo,

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         // B21 is good at everything
+        // i have already published on GitHub.com
+        //now i am ready to make more commits
+        //but do i need to share again????
+        // ABSOLUTELY NO
+        // IT'S TIME COMMIT AND JUST PUSH1
     }
 }
